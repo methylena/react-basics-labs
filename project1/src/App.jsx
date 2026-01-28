aba9ea31e91a27efb3f0a02520dd29816f26571d
Milena Korol <stupid.milena@gmail.com>
2026-01-28 15:49:57 +0000

    Rendered list of tasks using map

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -16,12 +16,13 @@ function App() {
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
-      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
-      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
-      <h1>{taskState.tasks[1].description}</h1>
-      <h1>{taskState.tasks[2].description}</h1>
-      <h1>{taskState.tasks[0].description}</h1>
+      {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+    />
+  ))} 
     </div>
   );
 
